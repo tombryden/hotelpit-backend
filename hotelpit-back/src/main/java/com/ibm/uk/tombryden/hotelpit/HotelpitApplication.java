263cd7c32880cbcb44fc45473b251b2fee2d552f
Tom Bryden <tombryden@me.com>
2022-03-15 23:22:50 +0000

    Initial commit

---
+++ hotelpit-back/src/main/java/com/ibm/uk/tombryden/hotelpit/HotelpitApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.uk.tombryden.hotelpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelpitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelpitApplication.class, args);
+	}
+
+}
---
+++ hotelpit-back/src/test/java/com/ibm/uk/tombryden/hotelpit/HotelpitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.uk.tombryden.hotelpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelpitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
