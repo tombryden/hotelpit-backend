88fd96e4553d8cc411c6cbcc8070ef16d1002fdc
Tom Bryden <tombryden@me.com>
2022-04-05 18:08:06 +0100

    Add rate to booking endpoint

--- hotelpit-back/src/main/java/com/ibm/uk/tombryden/hotelpit/controller/BookingController.java
+++ hotelpit-back/src/main/java/com/ibm/uk/tombryden/hotelpit/controller/BookingController.java
@@ -8,19 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.uk.tombryden.hotelpit.dto.PaymentDTO;
 import com.ibm.uk.tombryden.hotelpit.dto.ReservationDTO;
 import com.ibm.uk.tombryden.hotelpit.entity.Booking;
 import com.ibm.uk.tombryden.hotelpit.entity.Booking.BookingStatus;
+import com.ibm.uk.tombryden.hotelpit.entity.Rate;
 import com.ibm.uk.tombryden.hotelpit.entity.Room;
 import com.ibm.uk.tombryden.hotelpit.entity.User;
 import com.ibm.uk.tombryden.hotelpit.repository.BookingRepository;
+import com.ibm.uk.tombryden.hotelpit.repository.RateRepository;
 import com.ibm.uk.tombryden.hotelpit.repository.RoomRepository;
 import com.ibm.uk.tombryden.hotelpit.repository.UserRepository;
 import com.ibm.uk.tombryden.hotelpit.security.AuthenticatedUser;
@@ -41,6 +45,29 @@ public class BookingController {
 	@Autowired
 	private RoomRepository roomRepository;
 	
+	@Autowired
+	private RateRepository rateRepository;
+	
+	// FUNCTIONS
+	private ResponseEntity<Object> verifyBookingFromRequest(Optional<Booking> booking) {
+		// get authenticated user, get booking, check if booking is owned by current user, return booking
+		AuthenticatedUser authUser = new AuthenticatedUser();
+		Optional<User> user = authUser.getUserFromRepository(userRepository);
+		
+		// check if user isnt found, if so return 404 (should never occur)
+		if(user.isEmpty()) return ResponseEntity.status(404).body(new TextResponse("User not found"));
+		
+		// get booking
+		if(booking.isEmpty()) return ResponseEntity.status(404).body(new TextResponse("Booking not found"));
+		
+		// check if booking is owned by authorised user
+		if(!user.get().equals(booking.get().getUser())) return ResponseEntity.status(403).body(new TextResponse("The current booking is not owned by the authorised user"));
+		
+		// if all is successful, returnn null
+		return null;
+	}
+	
+	// MAPPINGS
 	@PostMapping("/pay")
 	public ResponseEntity<Object> payAndCreateBooking(@Valid @RequestBody PaymentDTO paymentDTO) {
 		// if card doesnt start with 4242 decline card number
@@ -106,6 +133,29 @@ public class BookingController {
 		if(!user.get().equals(booking.get().getUser())) return ResponseEntity.status(403).body(new TextResponse("The current booking is not owned by the authorised user"));
 		
 		return ResponseEntity.ok(booking.get());
+	}
+	
+	@PatchMapping("/{bookingid}/rate")
+	public ResponseEntity<Object> addRateToBooking(@PathVariable long bookingid, @RequestParam long rateid) throws InterruptedException {
+		Thread.sleep(5000);
 		
+		// get booking from id
+		Optional<Booking> booking = bookingRepository.findById(bookingid);
+		
+		ResponseEntity<Object> verifyResp = verifyBookingFromRequest(booking);
+		// if verified booking has an error return the error
+		if(verifyResp != null) return verifyResp;
+		
+		// verify rate exists and is part of the booking room rate list
+		Optional<Rate> rate = rateRepository.findById(rateid);
+		if(rate.isEmpty()) return ResponseEntity.status(404).body(new TextResponse("Rate not found"));
+		
+		// is not part of booking room rate list
+		if(!booking.get().getRoom().getRates().contains(rate.get())) return ResponseEntity.status(400).body(new TextResponse("Specified rate is not part of the booking room"));
+		
+		// checks done.. add rate to the booking
+		booking.get().setRate(rate.get());
+		
+		return ResponseEntity.ok(bookingRepository.save(booking.get()));
 	}
 }
