cec953d394c779e64448e3d5852875644f166bc0
Tom Bryden <tombryden@me.com>
2022-05-15 23:25:44 +0100

    Ran num fix

--- hotelpit-back/src/main/java/com/ibm/uk/tombryden/hotelpit/util/Utils.java
+++ hotelpit-back/src/main/java/com/ibm/uk/tombryden/hotelpit/util/Utils.java
@@ -2,7 +2,7 @@ package com.ibm.uk.tombryden.hotelpit.util;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Utils {
 	
@@ -20,8 +20,7 @@ public class Utils {
 	
 	
 	public static int getRandomNumber(int min, int max) {
-		Random rand = new Random();
-		return rand.nextInt((max - min) + min);
+		return ThreadLocalRandom.current().nextInt(min, max + 1);
 	}
 
 }
